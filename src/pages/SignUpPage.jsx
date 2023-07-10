2b3eda89724ceb498765a24302f8aba37bd7afe7
samufigh <samufigh21@gmail.com>
2023-07-09 21:39:08 -0300

    teste

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,32 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
 
 export default function SignUpPage() {
+  //useEffect(() => {
+  //  axios.get()
+  //})
+
+  function Register(e) {
+    console.log("oi");
+    const navigate = useNavigate();
+
+
+    e.preventDefault();
+    navigate("/");
+  }
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={Register}>
         <MyWalletLogo />
         <input placeholder="Nome" type="text" />
         <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input placeholder="Senha" type="password" autoComplete="new-password" />
+        <input placeholder="Confirme a senha" type="password" autoComplete="new-password" />
         <button>Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
